cb143d23a1577c29a7af702b606c4b1eb22a693c
dapaath <dafaathaullah123@gmail.com>
2022-03-14 12:22:46 +0700

    Menambahkan file r

--- coba_coba.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-data_train_1 = pd.read_csv("train_hp.csv")
-
-data_test = pd.read_csv("train_hp.csv")
---
+++ utama.R
@@ -0,0 +1,48 @@
+library(class)
+library(caret)
+
+data_train = read.csv("train_hp.csv")
+data_test = read.csv("test_hp.csv")
+# #######
+# set.seed(123)
+# n = 10000
+# a = sample(1:10, n, replace = T)
+# b = sample(10:20, n, replace = T)
+# f = ifelse(a > 5 & b > 10, "red", 
+#            ifelse(a < 3 | b < 4, "yellow", "green"))
+# 
+# df = data.frame(a = a, b = b, price_range = as.factor(f)) 
+# 
+# head(df)
+# #######
+
+#######
+data_train = read.csv("train_hp.csv")
+dim(data_train)
+
+index = createDataPartition(data_train$price_range, p = .8, list = F)
+
+trainData = data_train[index, ]
+testData = data_train[-index, ]
+
+dim(trainData)
+dim(testData)
+
+train = data.matrix(trainData[, 1:20])
+test = data.matrix(testData[, 1:20])
+
+summary(train)
+summary(test)
+
+train_label = factor(trainData[, "price_range"])
+test_label = factor(testData[, "price_range"])
+#######
+
+#######
+codeBook = lvqinit(train, train_label, size = 100)
+
+buildCodeBook = olvq1(train, train_label, codeBook, niter=100)
+
+predict = lvqtest(buildCodeBook, test)
+
+confusionMatrix(test_label, predict)
