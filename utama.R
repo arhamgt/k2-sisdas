ee90188ec0428bb5e32107597a861863218f3c77
rizal-mujahiddan69 <ganteng3rizal@apps.ipb.ac.id>
2022-03-16 11:26:09 +0700

    Tambahkan plot utama file

--- utama.R
+++ utama.R
@@ -1,5 +1,6 @@
 library(class)
 library(caret)
+library(ggplot)
 
 
 set.seed(123)
@@ -16,8 +17,14 @@ index = createDataPartition(data_train$price_range, p = .8, list = F)
 trainData = data_train[index, ]
 testData = data_train[-index, ]
 
+write.csv(trainData,"trainingData.csv",row.names = FALSE)
+write.csv(trainData,"testData.csv",row.names = FALSE)
+
 dim(trainData)
 dim(testData)
+
+head(trainData)
+head(testData)
 
 train = data.matrix(trainData[, 1:20])
 test = data.matrix(testData[, 1:20])
@@ -29,12 +36,23 @@ train_label = factor(trainData[, "price_range"])
 test_label = factor(testData[, "price_range"])
 #######
 
+accuracy_vektor <- c()
+epoch_vektor <- c()
 #######
-codeBook = lvqinit(train, train_label, size = 100)
-
-buildCodeBook = olvq1(train, train_label, codeBook, niter=100, alpha=0.2)
-
-predict = lvqtest(buildCodeBook, test)
-
-confusionMatrix(test_label, predict)
+for(i in seq(1600 , 1600000, 1600)){
+  codeBook = lvqinit(train, train_label, size = 100)
+  buildCodeBook = olvq1(train, train_label, codeBook, niter=i, alpha=0.2)
+  predict = lvqtest(buildCodeBook, test)
+  confusionMatrix(test_label, predict)
+  conmat <- confusionMatrix(test_label, predict)
+  
+  accuracy_vektor <- c(accuracy_vektor,conmat$overall["Accuracy"])
+  epoch_vektor <- c(epoch_vektor,i/1600)
+}
 #######
+data_plot           <- cbind(epoch_vektor,accuracy_vektor)
+data_plot           <- data.frame(data_plot)
+rownames(data_plot) <- NULL
+
+ggplot(data_plot , aes(x = epoch_vektor ,y=accuracy_vektor))+
+  geom_line()
