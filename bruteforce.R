e991aec41ab7264f3fde6b257b256bc9dffc7517
dapaath <dafaathaullah123@gmail.com>
2022-03-18 20:52:25 +0700

    Memperbaiki brute force dan merefactor kode dengan membuat file baru, LVQ.R

--- bruteforce.R
+++ bruteforce.R
@@ -1,53 +1,12 @@
-# Melakukan import library
-library(class)
-library(caret)
-library(ggplot2)
-
-# Melakukan seeding, seeding dilakukan sehingga tidak terjadi kejadian acak pada fungsi,
-# sehingga hasil akan selalu sama jika mempunyai input yang sama.
-set.seed(123)
-
-####### MEMBACA DAN PROSES DATA
-
-# Membuka file data dan membacanya ke program
-dataTrain = read.csv("train_hp.csv")
-dim(dataTrain) # [1] 2000   21
-
-# Membagi data menjadi data training dan data test
-index = createDataPartition(dataTrain$price_range, p = .8, list = F)
-trainData = dataTrain[index, ]
-testData = dataTrain[-index, ]
-dim(trainData) # [1] 1600   21
-dim(testData) # [1] 400  21
-
-# Mengeprint 5 data teratas dari dataframe data train dan test
-head(trainData)
-head(testData)
-
-# Mengubah dataframe menjadi matrix, sehingga bisa dilakukan proses selanjutnya,
-# dan juga menghilangkan label klasifikasi pada dataframe test dan train
-trainDataMatrix = data.matrix(trainData[, 1:20])
-testDataMatrix = data.matrix(testData[, 1:20])
-
-# Mengecek apakah data sudah benar
-summary(trainDataMatrix)
-summary(testDataMatrix)
-
-# Membuat vector yang menampung label data training dan test
-train_label = factor(trainData[, "price_range"])
-test_label = factor(testData[, "price_range"])
-
-
-
+# Melakukan import fungsi LVQ yang didapat pada file LVQ.R
+source('LVQ.R')
 
 ####### MELAKUKAN BRUTE FORCE
 ##### Brute force dilakukan untuk menentukan epoch dan alpha yang memiliki akurasi paling maksimal
-
 ### Melakukan pengaturan terhadap range yang perlu di bruteforce
-
 # Mengatur range epoch yang perlu di bruteforce
 awalEpoch   <- 1
-akhirEpoch <- 10
+akhirEpoch <- 100000
 incrementEpoch <- 100
 
 # Mengatur range alpha yang perlu di bruteforce
@@ -62,32 +21,15 @@ dataPlot <- data.frame(
   alphaVektor =  c()
 )
 
-
-
-
 # Melakukan looping untuk mencari epoch(niter) dan alpha yang memiliki akurasi paling baik
 for(niter in seq(awalEpoch*incrementEpoch , akhirEpoch*incrementEpoch , incrementEpoch)){
   # niter = 100, 200, 300, 400, ...
   for(alpha in seq(awalAlpha, akhirAlpha, incrementAlpha)){
     # alpha = 0.1, 0.3, 0.5, ...
     
-    #### Melakukan LVQ Utama
-    # Membuat codebook LVQ, codebook digunakan untuk memformat data sehingga bisa masuk 
-    # ke fungsi selanjutnya
-    codeBook = lvqinit(trainDataMatrix, train_label, size = 100)
-    
-    # Melakukan fungsi LVQ utama, disini kita memasukan codebook yang sudah dibuat sebelumnya,
-    # kita juga memasukan parameter epoch (niter) dan alpha awal. Fungsi ini menghasilkan 
-    # model hasil dari LVQ, berisi klasifikasi yang dihasilkan dari masing-masing data
-    classifiedCodebook = olvq1(trainDataMatrix, train_label, codeBook, niter=niter, alpha=alpha)
-    
-    # Melakukan testing model yang sudah dibuat terhadap data test. Menghasilkan variabel
-    # yang berisi hasil klasifikasi yang dilakukan.
-    predict = lvqtest(classifiedCodebook, testDataMatrix)
-    
-    # Mencocokan hasil klasifikasi yang dihasilkan dengan label test, kemudian
-    # membuat confusion matrix dari perbandingan tersebut.
-    conmat <- confusionMatrix(test_label, predict)
+    # Melakukan fungsi LVQ utama buatan kita yang sudah kita import. Dengan parameter niter 
+    # dan alpha yang sedang kita bruteforce.
+    conmat <- LVQ(niter, alpha)
 
     # Menambahkan baris baru pada dataframe akurasi dan mencatat epoch serta alpha.
     newDataPlot = data.frame(
@@ -101,7 +43,6 @@ for(niter in seq(awalEpoch*incrementEpoch , akhirEpoch*incrementEpoch , incremen
 #######
 
 ####### MENCARI ALPHA DAN EPOCH DENGAN AKURASI TERTINGGI
-
 # Menghilangkan rowname sehingga lebih rapih
 rownames(dataPlot) <- NULL
 # Melakukan plotting pengaruh epoch terhadap akurasi
@@ -111,6 +52,7 @@ ggplot(dataPlot , aes(x = epochVektor ,y=accuracyVektor))+
 ggplot(dataPlot , aes(x = alphaVektor ,y=accuracyVektor))+
   geom_line()
 
+print(dataPlot)
 # Mencari baris dataplot yang mempunyai akurasi yang paling besar
 print("Epoch Maksimum")
 print(dataPlot[which.max(dataPlot$accuracyVektor),])
--- main.R
+++ main.R
@@ -1,64 +1,9 @@
-# Melakukan import library
-library(class)
-library(caret)
-library(ggplot2)
-
-# Melakukan seeding, seeding dilakukan sehingga tidak terjadi kejadian acak pada fungsi,
-# sehingga hasil akan selalu sama jika mempunyai input yang sama.
-set.seed(123)
-
-####### MEMBACA DAN PROSES DATA
-
-# Membuka file data dan membacanya ke program
-dataTrain = read.csv("train_hp.csv")
-dim(dataTrain) # [1] 2000   21
-
-# Membagi data menjadi data training dan data test
-index = createDataPartition(dataTrain$price_range, p = .8, list = F)
-trainData = dataTrain[index, ]
-testData = dataTrain[-index, ]
-dim(trainData) # [1] 1600   21
-dim(testData) # [1] 400  21
-
-# Mengeprint 5 data teratas dari dataframe data train dan test
-head(trainData)
-head(testData)
-
-# Mengubah dataframe menjadi matrix, sehingga bisa dilakukan proses selanjutnya,
-# dan juga menghilangkan label klasifikasi pada dataframe test dan train
-trainDataMatrix = data.matrix(trainData[, 1:20])
-testDataMatrix = data.matrix(testData[, 1:20])
-
-# Mengecek apakah data sudah benar
-summary(trainDataMatrix)
-summary(testDataMatrix)
-
-# Membuat vector yang menampung label data training dan test
-train_label = factor(trainData[, "price_range"])
-test_label = factor(testData[, "price_range"])
-####### 
-
-
-
-####### MELAKUKAN LVQ
-#### Melakukan LVQ Utama
-# Membuat codebook LVQ, codebook digunakan untuk memformat data sehingga bisa masuk 
-# ke fungsi selanjutnya
-codeBook = lvqinit(trainDataMatrix, train_label, size = 100)
-
-# Melakukan fungsi LVQ utama, disini kita memasukan codebook yang sudah dibuat sebelumnya,
-# kita juga memasukan parameter epoch (niter) dan alpha awal. Fungsi ini menghasilkan 
-# model hasil dari LVQ, berisi klasifikasi yang dihasilkan dari masing-masing data
-niter = 900
-alpha = 0.7
-classifiedCodebook = olvq1(trainDataMatrix, train_label, codeBook, niter=niter, alpha=alpha)
-
-# Melakukan testing model yang sudah dibuat terhadap data test. Menghasilkan variabel
-# yang berisi hasil klasifikasi yang dilakukan.
-predict = lvqtest(classifiedCodebook, testDataMatrix)
-
-# Mencocokan hasil klasifikasi yang dihasilkan dengan label test, kemudian
-# membuat confusion matrix dari perbandingan tersebut.
-conmat <- confusionMatrix(test_label, predict)
+# Kita mendapatkan bahwa 
+# niter = xxx
+# alpha = xxx
+# merupakan parameter yang menghasilkan akurasi tertinggi
+niter = 1000
+alpha = 0.3
+cat(sprintf("Matriks perbandingan dari hasil LVQ dengan niter = %d dan alpha = %f\n", niter, alpha))
+conmat <- LVQ(1000, 0.3)
 print(conmat)
-#######
